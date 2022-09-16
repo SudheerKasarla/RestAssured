a63849a52bf660b6472b5db47ad6a942b0490833
sudheerkasarla <sudheerksrgdk@gmail.com>
2022-09-16 13:11:23 +0530

    RestAPI Mergers.

--- src/test/java/com/restassured/GetRequest.java
+++ src/test/java/com/restassured/GetRequest.java
@@ -6,32 +6,24 @@ public class GetRequest {
 
 	public static void main(String[] args) {
 
-
 		/*
 		 * 1) Build Request 
 		 * 2) Hit Request and get Response 
 		 * 3) Validate Response
 		 */
-		
-		RestAssured
-			.given()
-				.log()
-				.all()
-				.baseUri("https://restful-booker.herokuapp.com/")
-				.basePath("booking/{id}")
-				
-				.pathParam("id", 10)
-			.when()
-				.get()
-			.then()
-				.log()
-				.all()
-				.statusCode(200);
-				
-			
-		
-		
-		
+
+		RestAssured.given()
+							.log()
+							.all()
+							.baseUri("https://restful-booker.herokuapp.com/")
+							.basePath("booking/{id}")
+							.pathParam("id", 10)
+					.when()
+							.get()
+					.then()
+							.log().all()
+							.statusCode(200);
+
 	}
 
 }
